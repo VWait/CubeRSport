750fa014c2802cbbdeb56ad83b41a5294b33d720
vlnovikov <v.novikov@clevermis.ru>
2024-12-12 12:40:12 +0500

    Похуй, в мейн ветку ебашу

--- cube_r_sport/cube_r_sport/settings.py
+++ cube_r_sport/cube_r_sport/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'cube_r_sport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            BASE_DIR / 'cube_r_sport/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
